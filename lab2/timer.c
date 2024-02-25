297b1649f6255fe07ee6833b430bd0ee9025dd35
Rui Borges <up202207475@up.pt>
2024-02-25 01:51:15 +0000

    Updates to finish timer_display_conf

--- lab2/timer.c
+++ lab2/timer.c
@@ -32,16 +32,91 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+    //Read-back command
+    uint8_t rb=0;
+    rb += BIT(0); // RESERVED FIELD
+    
+    //Select counter
+    if (timer==0){
+      rb |= TIMER_SEL0;
+    }else if (timer==1){
+      rb |= TIMER_SEL1;
+    }else if (timer==2){
+      rb |= TIMER_SEL2;
+    }
+   rb |= TIMER_RB_STATUS_; // Read programmed mode
+   rb |= TIMER_RB_COUNT_; // Read count value 
+   rb |= TIMER_RB_CMD; //Read back command
 
-  return 1;
+   if(sys_outb(TIMER_CTRL,rb)!=0){
+    return 1;
+   }
+   
+   switch(timer){
+    case 0:
+      if(util_sys_inb(TIMER_0,st)){
+        return 1;
+      }
+      break;
+    case 1:
+      if(util_sys_inb(TIMER_1,st)){
+        return 1;
+      }
+      break;
+    case 2:
+      if(util_sys_inb(TIMER_2,st)){
+        return 1;
+      }
+      break;
+   }
+  return 0;
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+
+  union timer_status_field_val val;
+
+  switch (field){
+
+    case tsf_base:
+      if (st&TIMER_BCD){
+        val.bcd = true;
+      }else{
+        val.bcd = false;
+      }
+      break;
+
+    case tsf_mode:
+      if (st&TIMER_SQR_WAVE){
+        val.count_mode = 3;
+      }else if (st&TIMER_RATE_GEN){
+        val.count_mode = 2;
+      }else {
+        val.count_mode = 1;
+      }
+      break;
+
+    case tsf_initial:
+      if (st&TIMER_LSB){
+        val.in_mode = LSB_only;
+      }else if (st&TIMER_MSB){
+        val.in_mode = MSB_only;
+      }else if (st&TIMER_LSB_MSB){
+        val.in_mode = MSB_after_LSB;
+      }
+      break;
+
+    case tsf_all:
+      val.byte = st;
+      break;
+      
+    default:
+      return 1;
+      break;
+  }
+
+  if(timer_print_config(timer,field,val)!=0) return 1;
+  return 0;
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  // Test
+
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
 
@@ -18,8 +18,8 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint32_t val;
+  int return_value = sys_inb(port, &val);
+  *value = (uint8_t)val;
+  return return_value;
 }

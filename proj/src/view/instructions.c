9195ab3e7bc8de6ea938f1f6a3d2099f020ac14f
Maria Rabelo <up202000130@up.pt>
2024-05-29 19:52:32 +0100

    better instructions centralizing

--- proj/src/view/instructions.c
+++ proj/src/view/instructions.c
@@ -7,12 +7,12 @@
 
 const char* game_instructions = 
     "\n\n"
-    "                        WELCOME TO TYPING TUTOR GAME! :) \n\n\n"
-    "                                HOW TO PLAY:\n\n"
-    "                        TYPE AS FAST AS YOU CAN! \n"
-    "                   USE THE SPACEBAR TO ADVANCE WORDS. \n"
-    "                 USE BACKSPACE TO UNDO THE LAST LETTER. \n"
-    "                          PRESS ANYWHERE TO GO BACK. \n\n"
-    "                                GOOD LUCK! \n";
+    "                       WELCOME TO TYPING TUTOR GAME! :) \n\n\n"
+    "                               HOW TO PLAY:\n\n"
+    "                       TYPE AS FAST AS YOU CAN! \n"
+    "                  USE THE SPACEBAR TO ADVANCE WORDS. \n"
+    "                USE BACKSPACE TO UNDO THE LAST LETTER. \n"
+    "                         PRESS ANYWHERE TO GO BACK. \n\n"
+    "                               GOOD LUCK! \n";
 
 

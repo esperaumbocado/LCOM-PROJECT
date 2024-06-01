6598d694db9bfb015c3e3d5c59e06cf253847fc5
Rui Borges <ruiborges206@gmail.com>
2024-06-01 14:38:23 +0100

    Moved Animation struct to sprite.h

--- proj/src/model/sprite.h
+++ proj/src/model/sprite.h
@@ -13,6 +13,17 @@ typedef struct {
     uint32_t color;
 } Sprite; 
 
+
+typedef struct {
+    Sprite **frames;
+    int frameCount;
+    int currentFrame; 
+    bool isActive; 
+    int frameDuration;
+    int frameCounter;
+} Animation;
+
+
 Sprite *create_sprite_xpm(xpm_map_t sprite, uint16_t x, uint16_t y);
 Sprite *create_sprite_button(uint16_t width, uint16_t height, uint32_t color);
 void destroy_sprite(Sprite *sprite);

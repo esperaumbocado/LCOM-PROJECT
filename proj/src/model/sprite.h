a061da07fbb3e804de1eea7095b3e44b98d18bab
Rui Borges <ruiborges206@gmail.com>
2024-06-01 15:05:05 +0100

    Added comments to sprite.h

--- proj/src/model/sprite.h
+++ proj/src/model/sprite.h
@@ -4,6 +4,16 @@
 #
 #include "../drivers/graphics/video.h"
 
+
+/**
+ * @brief Struct that represents a sprite
+ * @param height the height of the sprite
+ * @param width the width of the sprite
+ * @param x the x position of the sprite (Only used for buttons)
+ * @param y the y position of the sprite (Only used for buttons)
+ * @param colors the colors of the sprite
+ * @param color the color of the sprite
+*/
 typedef struct {
     uint16_t height;
     uint16_t width;
@@ -14,6 +24,16 @@ typedef struct {
 } Sprite; 
 
 
+
+/**
+ * @brief Struct that represents an animation
+ * @param frames the frames of the animation
+ * @param frameCount the number of frames
+ * @param currentFrame the current frame
+ * @param isActive if the animation is active
+ * @param frameDuration the duration of each frame
+ * @param frameCounter the counter of the frames
+*/
 typedef struct {
     Sprite **frames;
     int frameCount;
@@ -24,8 +44,19 @@ typedef struct {
 } Animation;
 
 
+/**
+ * @brief Creates a sprite from an xpm
+ * @param sprite the xpm of the sprite
+ * @param x the x position of the sprite
+ * @param y the y position of the sprite
+*/
 Sprite *create_sprite_xpm(xpm_map_t sprite, uint16_t x, uint16_t y);
-Sprite *create_sprite_button(uint16_t width, uint16_t height, uint32_t color);
+
+
+/**
+ * @brief Destroys a sprite
+ * @param sprite the sprite to be destroyed
+*/
 void destroy_sprite(Sprite *sprite);
 
 #endif
